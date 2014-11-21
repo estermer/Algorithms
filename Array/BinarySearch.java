7462f11517c19a36a9f5c73e5c7deef9a56bd6f1
Nitish Devadiga <nitish.m.d@gmail.com>
2014-11-21 10:37:39 -0500

    updated binary search with increase dec

--- Array/BinarySearch.java
+++ Array/BinarySearch.java
@@ -25,30 +25,23 @@ public class BinarySearch
     return -1;
   }
   
-  public static int decIncSearch(int[] a)
-  {
+  public static int incDecSearch(int[] a) {
     int low = 0;
-    int mid = 0;
     int high = a.length - 1;
     
-    if(low > 0)
-      return a[low];
+    while(low <= high) {
+      if(low == high)
+        return a[low];
+      if(low == high - 1)
+        return Math.max(a[low], a[high]);
 
-    if (low == high && low > 0)
-      return 0;
-
-    if (high < 0)
-      return -1;
-
-    while (low <= high)
-    {
-      mid = low + (high - low)/2;
-      if (a[mid] > a[mid - 1] && a[mid - 1] < 0 && a[mid] > 0)
+      int mid = low + (high - low)/2;
+      if(a[mid] > a[mid - 1] && a[mid] > a[mid + 1])
         return a[mid];
-      else if (a[mid] > a[mid - 1] && a[mid - 1] > 0)
+      else if(a[mid] > a[mid - 1] && a[mid] < a[mid + 1])
+        low = mid + 1;
+      else if(a[mid] < a[mid - 1] && a[mid] > a[mid + 1])
         high = mid - 1;
-      else
-        low = mid + 1; 
     }
     return -1;
   }
@@ -56,8 +49,8 @@ public class BinarySearch
   public static void main(String[] args)
   {
     int[] arr = {1,2,3,4,5,6,7};
-    int[] arr2 = {-5,-2,-3,-7,-8,-10,15}; 
+    int[] arr2 = {10, 11, 12, 13, 9, 8, 7, 4};
     //System.out.println(sortedSearch(arr, 7));
-    System.out.println(decIncSearch(arr2)); 
+    System.out.println(incDecSearch(arr2)); 
   }
 }
